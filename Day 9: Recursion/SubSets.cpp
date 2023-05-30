b00ece732488212f6a97796c56ec013394b1719b
yatraavaghasia <113079616+yatraavaghasia@users.noreply.github.com>
2023-05-30 10:38:50 +0530

    https://practice.geeksforgeeks.org/problems/subset-sums2234/1

---
+++ Day 9: Recursion/SubSets.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    void SubsetSumsHelper(int index, vector <int>&arr, int n, vector <int>&ans, int sum)
+    {
+        if(index==n)
+        {
+            ans.push_back(sum);
+            return;
+        }
+        
+        SubsetSumsHelper(index+1,arr,n,ans,sum+arr[index]);
+        
+        SubsetSumsHelper(index+1,arr,n,ans,sum);
+    }
+    vector<int> subsetSums(vector<int> arr, int N)
+    {
+        vector <int> ans;
+        int sum=0;
+        SubsetSumsHelper(0,arr,N,ans,sum);
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        vector<int> arr(N);
+        for(int i = 0 ; i < N ; i++){
+            cin >> arr[i];
+        }
+        Solution ob;
+        vector<int> ans = ob.subsetSums(arr,N);
+        sort(ans.begin(),ans.end());
+        for(auto sum : ans){
+            cout<< sum<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
